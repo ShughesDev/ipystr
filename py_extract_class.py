2e2254e36b08b6c1489e82fb8abd8fef073d8a96
Sam <samjfhughes@gmail.com>
2022-04-01 20:07:38 +0100

    added unit tests etc

--- py_extract_class.py
+++ py_extract_class.py
@@ -7,24 +7,48 @@ class py_extract:
     def __init__(self,path = None):
         self.path = path
         try:
-            self.extract()
+            self.routine()
+            
         except:
             print("Initialised pyextract with no path.")
+    
+    def routine(self):
+        self.extract()
+        self.filter_imports()
         
+    
     def extract(self):
         self.code = []
         for line in open(self.path,"r"):
             self.code.append(line)
             
-        print(self.code)
+        #print(self.code)
     
-    
+    def filter_imports(self):
         
-
+        self.import_lines = []
+        
+        for line in self.code:
+            if len(line.split("import")) > 1:
+                self.import_lines.append(line.split("\n")[0])
+    
+    def filter_frm_std(self):
+        
+        self.frm = []
+        self.std = []
+        
+        for line in self.import_lines:
+            
+            if len(line.split("from"))>1:
+                self.frm.append(line)
+            else:
+                self.std.append(line)
+        
+'''
 path = "sample/library_ex/script_b.py"
 
 alpha = py_extract(path)
-
+'''
 
 '''
 sample = modules[3]
--- py_extract_class_UT.py
+++ py_extract_class_UT.py
@@ -3,4 +3,56 @@ python script import extractor
 UT
 '''
 
+#################### Imports
+
 import pytest
+
+from py_extract_class import py_extract
+
+#################### Tests
+
+def test_py_extract_filter_imports():
+    
+    alpha = py_extract()
+    
+    code_sample = ["import matplotlib.pyplot as plt\n",
+                   "from a import b\n",
+                   "\n",
+                   "print('test')"]
+    
+    alpha.code = code_sample
+    
+    alpha.filter_imports()
+    
+    actual = alpha.import_lines
+    
+    expected = ["import matplotlib.pyplot as plt",
+                "from a import b"]
+    
+    print(actual)
+    
+    for i in range(len(actual)):
+        assert actual[i] == expected[i]
+        
+def test_py_extract_filter_frm_std():
+    
+    alpha = py_extract()
+    
+    import_lines = ["import matplotlib.pyplot as plt",
+                "from a import b"]
+    
+    alpha.import_lines = import_lines
+    
+    expected_std = ["import matplotlib.pyplot as plt"]
+    expected_frm = ["from a import b"]
+    
+    alpha.filter_frm_std()
+    
+    actual_std = alpha.std
+    actual_frm = alpha.frm
+    
+    for i in range(len(actual_std)):
+        assert actual_std[i] == expected_std[i]
+        
+    for i in range(len(actual_frm)):
+        assert actual_frm[i] == expected_frm[i]
